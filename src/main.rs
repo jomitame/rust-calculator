395ab872d46b23f9adba6c8802e0cc99ed24071c
jomitame <jomitame@gmail.com>
2023-01-19 14:22:24 -0500

    create a multiple adition like 10+11+12

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use regex::Regex;
 
 fn main() {
-    println!("Hello, world!");
-
     // Regex
     let regex_for_add: Regex = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
 
@@ -11,14 +9,28 @@ fn main() {
     let mut expression: String = String::new();
     std::io::stdin().read_line(&mut expression).unwrap();
 
-    // Operations
-    let caps = regex_for_add.captures(expression.as_str()).unwrap();
-    let firts_number: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-    let second_number: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
-    println!("{:?} firts: {}, second: {}", caps, firts_number, second_number);
+    // loop for more thar two numbers
+    // Adition
+    loop {
+        // Operations
+        let caps = regex_for_add.captures(expression.as_str());
 
-    let addition: i32 = firts_number + second_number;
+        if caps.is_none() {
+            break;
+        }
+
+        let caps = caps.unwrap();
+
+        let cap_expression: &str = caps.get(0).unwrap().as_str();
+        let firts_number: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
+        let second_number: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
+
+        let addition: i32 = firts_number + second_number;
+
+        expression = expression.replace(cap_expression, &addition.to_string());
+        
+    }
 
     // Results
-    println!("Adition: {}", addition);
+    println!("Adition: {}", expression);
 }
