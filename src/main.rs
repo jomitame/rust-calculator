80d145fc319111b0c5e5c454396a9a88231c78a9
jomitame <jomitame@gmail.com>
2023-01-19 18:05:18 -0500

    refactor in separated functions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
 use regex::Regex;
 
+fn do_operation (regx: Regex, mut expression: String, operation: &str) -> String {
+    if operation.is_empty() {
+        return "".to_string();
+    }
+
+    // loop for more than two numbers
+    loop {
+        // Operations
+        let caps = regx.captures(expression.as_str());
+
+        if caps.is_none() {
+            break;
+        }
+        let caps = caps.unwrap();
+
+        let cap_expression = caps.get(0).unwrap().as_str();
+        let firts_number: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
+        let second_number: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
+
+        let result = match operation {
+            "+" => firts_number + second_number,
+            "*" => firts_number * second_number,
+            _ => 0,
+        };
+
+        expression = expression.replace(cap_expression, &result.to_string());
+    }
+    expression
+}
+
 fn main() {
     // Regex
     let regex_for_add: Regex = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
@@ -10,47 +40,11 @@ fn main() {
     let mut result: String = String::new();
     std::io::stdin().read_line(&mut result).unwrap();
 
-    // loop for more thar two numbers
     // Multiply
-    loop {
-        // Operations
-        let caps = regex_for_mult.captures(result.as_str());
-
-        if caps.is_none() {
-            break;
-        }
-
-        let caps = caps.unwrap();
-
-        let cap_expression: &str = caps.get(0).unwrap().as_str();
-        let firts_number: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-        let second_number: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
-
-        let multiply: i32 = firts_number * second_number;
-
-        result = result.replace(cap_expression, &multiply.to_string());
-        
-    }
-    // Adition
-    loop {
-        // Operations
-        let caps = regex_for_add.captures(result.as_str());
-
-        if caps.is_none() {
-            break;
-        }
-
-        let caps = caps.unwrap();
-
-        let cap_expression: &str = caps.get(0).unwrap().as_str();
-        let firts_number: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-        let second_number: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
-
-        let addition: i32 = firts_number + second_number;
-
-        result = result.replace(cap_expression, &addition.to_string());
-        
-    }
+    result = do_operation(regex_for_mult, result, "*");
+    // Sum
+    result = do_operation(regex_for_add, result, "+");
+    
 
     // Results
     println!("Result: {}", result);
